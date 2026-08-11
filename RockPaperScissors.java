9dc8312d8f38b81f572dfce8120288fa457a45cb
rozhinhesadi <rojinh84@gmail.com>
2026-08-11 22:57:56 +0330

    rewrite thr code and add ENUM instead of separate class for game

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,102 +1,113 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         System.out.println("hello welcome to our game");
-        System.out.println("for how many match you want to play? please enter a number");
-        boolean time = false;
-        int number = 0;
-        while (!time) {
+        System.out.println("for how many matches you want to play? please enter a number");
+
+        int numberOfRounds = 0;
+        boolean validNumber = false;
+        while (!validNumber) {
             try {
                 String input = userInput.nextLine();
-                number = Integer.parseInt(input);
-                time = true;
+                numberOfRounds = Integer.parseInt(input);
+                validNumber = true;
             } catch (NumberFormatException e) {
                 System.out.println("Invalid number!");
             }
         }
-        
-        
+
         Play play = new Play();
+        PcPlay pc = new PcPlay();
 
-        for (int i = 0; i < number; i++) {
-            boolean checkInput = false;
+        for (int i = 0; i < numberOfRounds; i++) {
+            Move userMove = null;
+
             System.out.println("enter your move use emoji");
-            System.out.println("Choose: 🗿 📄 ✂️");
-            String UserMove = "";
-            while (!checkInput) {
-                try {
-                    UserMove = userInput.nextLine();
+            System.out.println("Choose: " + Move.ROCK.getEmoji() + " " + Move.PAPER.getEmoji() + " " + Move.SCISSORS.getEmoji());
 
-                    if (!UserMove.equals("✂️") &&
-                        !UserMove.equals("📄") &&
-                        !UserMove.equals("🗿")) {
-
-                        throw new Exception("Invalid input");
-                    }
-
-                    checkInput = true;
-
-                } catch (Exception e) {
+            while (userMove == null) {
+                String input = userInput.nextLine();
+                userMove = Move.fromEmoji(input);
+                if (userMove == null) {
                     System.out.println("Invalid input");
                 }
             }
 
-            PcPlay system = new PcPlay();
-
-            String systemMove = system.setPosition();
-
-            play.game(UserMove, systemMove);
-
+            Move systemMove = pc.setPosition();
+            play.game(userMove, systemMove);
         }
-        
-        System.out.println("Your score: " + play.user_win);
-        System.out.println("Computer score: " + play.system_win);
 
-        if (play.user_win > play.system_win) {
+        System.out.println("Your score: " + play.userWin);
+        System.out.println("Computer score: " + play.systemWin);
+
+        if (play.userWin > play.systemWin) {
             System.out.println("You won!");
-        } else if (play.system_win > play.user_win) {
+        } else if (play.systemWin > play.userWin) {
             System.out.println("Computer won!");
         } else {
             System.out.println("Draw!");
         }
 
+        userInput.close();
     }
 }
-class Play{
-    int user_win = 0;  
-    int system_win = 0;  
-    public void game(String user_move, String system_move){
-        if ((user_move.equals("✂️") && system_move.equals("📄")) || (user_move.equals("🗿") && system_move.equals("✂️")) || (user_move.equals("📄") && system_move.equals("🗿"))){
-            user_win++;
-             System.out.println("You win this round!");
+
+enum Move {
+    ROCK("🗿"), PAPER("📄"), SCISSORS("✂️");
+
+    private final String emoji;
+
+    Move(String emoji) {
+        this.emoji = emoji;
+    }
+
+    public String getEmoji() {
+        return emoji;
+    }
+
+    public static Move fromEmoji(String emoji) {
+        for (Move m : values()) {
+            if (m.emoji.equals(emoji)) {
+                return m;
+            }
         }
-        else if (user_move.equals(system_move)){
-            System.out.println("your move is similar with system");
+        return null;
+    }
+}
+
+class Play {
+    int userWin = 0;
+    int systemWin = 0;
+
+    public void game(Move userMove, Move systemMove) {
+        if (userMove == systemMove) {
+            System.out.println("Your move is similar to system's");
+            return;
         }
-        else{
-            system_win++;
+
+        boolean userWins =
+            (userMove == Move.ROCK && systemMove == Move.SCISSORS) ||
+            (userMove == Move.SCISSORS && systemMove == Move.PAPER) ||
+            (userMove == Move.PAPER && systemMove == Move.ROCK);
+
+        if (userWins) {
+            userWin++;
+            System.out.println("You win this round!");
+        } else {
+            systemWin++;
             System.out.println("Computer wins this round!");
         }
-        
     }
-    
-    
 }
-class PcPlay{
-    Random random = new Random();
-    //int num = random.nextInt(3);
-    public String setPosition(){
-        int num = random.nextInt(3);
-        if(num == 0){
-            return "🗿";
-        }
-        else if(num == 1){
-            return "✂️";
-        }
-        return "📄";
-    }
-        
 
+class PcPlay {
+    private final Random random = new Random();
+
+    public Move setPosition() {
+        Move[] moves = Move.values();
+        return moves[random.nextInt(moves.length)];
+    }
 }
